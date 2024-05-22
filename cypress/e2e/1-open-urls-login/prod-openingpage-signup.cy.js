06e8605e8d22851cbc960f303c713c1384cd83f4
jinciy-a <jinciya@zoiteckh.com>
2024-05-22 17:12:20 +0530

    test change

--- cypress/e2e/1-open-urls-login/prod-openingpage-signup.cy.js
+++ cypress/e2e/1-open-urls-login/prod-openingpage-signup.cy.js
@@ -4,6 +4,6 @@ describe('openingpage', function(){
         cy.visit("https://projector.app/")
         cy.url().should("eq","https://projector.app/") // to verify
         cy.get('.MuiGrid-container > :nth-child(1) > .blue').click();
-        cy.url().should("eq","https://projector.app/newSign-up");
+        cy.url().should("eq","https://projector.app/newSign-up"); //hub
    })
 })
