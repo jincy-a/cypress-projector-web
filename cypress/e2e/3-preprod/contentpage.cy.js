86ac5f375c98fb7a44e2feb6a0ad9ec238101af9
jinciy-a <jinciya@zoiteckh.com>
2024-05-23 20:33:50 +0530

    preprod robs account

--- cypress/e2e/3-preprod/contentpage.cy.js
+++ cypress/e2e/3-preprod/contentpage.cy.js
@@ -5,16 +5,16 @@ describe('contentdashboard', function(){
         cy.visit("https://preprod.projector.app/");
         cy.get(".transparent").click(); 
         cy.url().should("eq","https://preprod.projector.app/newSign-in");
-        cy.get(".input-field_inputField__3cdaW").type("layne256@yopmail.com").should("have.value","layne256@yopmail.com");
+        cy.get(".input-field_inputField__3cdaW").type("robmarks@me.com").should("have.value","robmarks@me.com");
         cy.wait(2000);
         cy.get(".button_blockText__3MB5s").click();
-        cy.get("#password-input").type("Projectorapp@2024");
+        cy.get("#password-input").type("JuliaMarks1732!");
         cy.get("#sasdasddd").click();
 
-        cy.wait(10000);
-        cy.get(".header_profileMenuLink__3S2_U").click();
-        cy.wait(10000);
-        cy.get('[href="/your-videos"]').click();
+        cy.wait(20000);
+        cy.get(".header_options__2SF7N > .header_profileButton__2D8lI.header_regularView__1DUae > .header_profileMenuCurrentProfileDetailsIcon__2Iwlh").click();
+        cy.get(".header_profileMenuLinksContainer__3821f > [href='/dashboard']").click();
+        cy.get("[href='/your-videos']").click();
         cy.url().should("eq","https://preprod.projector.app/your-videos");
    })
 })
--- cypress/e2e/3-preprod/upload.cy.js
+++ cypress/e2e/3-preprod/upload.cy.js
@@ -5,10 +5,10 @@ describe('upload', function(){
         cy.visit("https://preprod.projector.app/");
         cy.get(".transparent").click(); 
         cy.url().should("eq","https://preprod.projector.app/newSign-in");
-        cy.get(".input-field_inputField__3cdaW").type("layne256@yopmail.com").should("have.value","layne256@yopmail.com");
+        cy.get(".input-field_inputField__3cdaW").type("robmarks@me.com").should("have.value","robmarks@me.com");
         cy.wait(2000);
         cy.get(".button_blockText__3MB5s").click();
-        cy.get("#password-input").type("Projectorapp@2024");
+        cy.get("#password-input").type("JuliaMarks1732!");
         cy.get("#sasdasddd").click();
 
         //cy.get(".input-field_errrMessage__BO92z").should("have.text","( Case Sensitive )Incorrect password");
